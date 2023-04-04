711513742d18cb5c789d85487c0b50bb7ee91113
Pablo Soriano <soriano.pablo@gmail.com>
2023-04-04 12:44:14 -0600

    Se modifica proyecto para publicarlo en heroky y netlfy, se instala dotenv para variables.env, se modifica package para produccion

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config({ path: 'variables.env'});
 
 // Cors permite que un cliente se conecte a otro servidor, para intercambio de recursos.
 const cors = require('cors');
 
 // conectar mongodb
 mongoose.Promise =global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/restapis', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true
 });
 
@@ -20,8 +21,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//VALIDACIONES PARA PERMITIR UNO O VARIOS DOMINIOS EN LA API
+
+// Definir un dominio(s) para recibir las peticiones (lista blanca)
+//const whitelist = ['http://localhost:3000'];
+const whitelist = [process.env.FRONTEND_URL];
+
+// opciones de cors:
+const corsOptions = {
+    origin: (origin, callback) => {
+      // console.log(origin);
+        // Revisar si la peticion viene de un servidor que esta en whiteList
+        const existe = whitelist.some( dominio => dominio === origin);
+        if(existe) {
+            callback(null, true)
+        } else {
+            // si no esta en whitelist
+            callback(new Error('No permitido por CORS'));
+        }
+
+    }
+}
+
 // Habilitar Cors
-app.use(cors());
+app.use(cors(corsOptions));  //le colocamos corsOptions cuando habilitamos la opcion de validar los dominios
 
 
 // Rutas de la app
@@ -31,12 +54,10 @@ app.use('/', routes());
 app.use(express.static('uploads'));
 
 
-
-
-
-
-
-
-
 // Puerto
-app.listen(5000);
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+//app.listen(5000);
+app.listen(port, host, () =>{
+    console.log('El Servidor está funcionando');
+})
--- routes/index.js
+++ routes/index.js
@@ -12,54 +12,54 @@ module.exports = function() {
 
 // Rutas de Clientes
     //Agrega nuevos clientes via POST
-    router.post('/clientes', clienteController.nuevoCliente);
+    router.post('/clientes', auth, clienteController.nuevoCliente);
 
     //Muestra todos los clientes
     router.get('/clientes', auth, clienteController.mostrarClientes);
 
     // Mostrar un cliente por medio de su id
-    router.get('/clientes/:idCliente', clienteController.mostrarCliente);
+    router.get('/clientes/:idCliente', auth, clienteController.mostrarCliente);
 
     // Actualizar un cliente
-    router.put('/clientes/:idCliente', clienteController.actualizarCliente);
+    router.put('/clientes/:idCliente', auth, clienteController.actualizarCliente);
 
     // Eliminar un cliente
-    router.delete('/clientes/:idCliente', clienteController.eliminaCliente);
+    router.delete('/clientes/:idCliente', auth, clienteController.eliminaCliente);
 
 // Rutas de Productos
     // Agrega nuevos productos via POST
-    router.post('/productos', productosController.subirArchivo ,productosController.nuevoProducto);
+    router.post('/productos',  productosController.subirArchivo ,productosController.nuevoProducto);
 
     // Muestra todos los productos
-    router.get('/productos', productosController.mostrarProductos);
+    router.get('/productos', auth, productosController.mostrarProductos);
 
     // Muestra un producto especifico por su ID
-    router.get('/productos/:idProducto', productosController.mostrarProducto);
+    router.get('/productos/:idProducto', auth, productosController.mostrarProducto);
 
     // Actualizar un Producto
-    router.put('/productos/:idProducto', productosController.subirArchivo, productosController.actualizarProducto);
+    router.put('/productos/:idProducto', auth, productosController.subirArchivo, productosController.actualizarProducto);
 
     // Eliminar Productos
     router.delete('/productos/:idProducto', productosController.eliminarProducto);
 
     // Busqueda de productos
-    router.post('/productos/busqueda/:query', productosController.buscarProducto);
+    router.post('/productos/busqueda/:query',  productosController.buscarProducto);
 
 // Rutas de Pedidos
     // Agregar nuevos pedidos via POST
-    router.post('/pedidos/nuevo/:idUsuario', pedidosController.nuevoPedido);
+    router.post('/pedidos/nuevo/:idUsuario',  pedidosController.nuevoPedido);
 
     // Mostrar todos los pedidos
-    router.get('/pedidos', pedidosController.mostrarPedidos);
+    router.get('/pedidos',  pedidosController.mostrarPedidos);
 
     //Mostrar un pedido por su ID
-    router.get('/pedidos/:idPedido', pedidosController.mostrarPedido);
+    router.get('/pedidos/:idPedido',  pedidosController.mostrarPedido);
 
     // Actualizar Pedidos
-    router.put('/pedidos/:idPedido', pedidosController.actualizarPedido);
+    router.put('/pedidos/:idPedido',  pedidosController.actualizarPedido);
 
     // Eliminar pedido
-    router.delete('/pedidos/:idPedido', pedidosController.eliminarPedido);
+    router.delete('/pedidos/:idPedido',  pedidosController.eliminarPedido);
 
 
     // Usuarios
